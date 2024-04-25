9f99abaf2f42a3310c3509679935a06a441e2a93
Da2004sha <darya.stepanova.2004_12@mail.ru>
2024-04-25 17:27:41 +0300

    Sixth commit

---
+++ src/main/java/com/application/NotesApp/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.application.NotesApp;
+
+import com.application.NotesApp.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
